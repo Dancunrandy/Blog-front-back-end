408ebf689bb4014897a3ddb0377fe892fbb8da07
dancunrandy <dancunrandy@gmail.com>
2024-08-28 12:00:56 -0400

    create and delete

--- app/database/db.php
+++ app/database/db.php
@@ -68,17 +68,61 @@ function selectOne($table, $conditions)
         $sql = $sql . " LIMIT 1";
         $stmt = executeQuery($sql,$conditions);
         $records = $stmt->get_result()->fetch_assoc();  
-        return $records;   
-        
-        
+        return $records;       
 
 }
 
-$conditions = [
-    'admin' => 0,
-    'username'  => 'dante'
+function create($table, $data)
+{
+    global $conn;
+    $sql = "INSERT INTO users SET ";
+    
+    $i = 0;
+    foreach ($data as $key => $value) {
+        if ($i === 0){
+            $sql = $sql . " $key=?";
+        } else {
+            $sql = $sql . ", $key=?";
+        }
+        $i++;
+    }
+    
+    $stmt = executeQuery($sql, $data);  // Pass the data to executeQuery
+    $id = $stmt->insert_id;
+    return $id;
+}
+
+
+function update($table,$id, $data)
+{
+        global $conn;
+    // $sql = " UPDATE users SET username=?,admin=?,email=?,password=? WHERE id=?"
+        $sql = "UPDATE $table SET ";
+        
+        $i = 0;
+        foreach ($data as $key => $value) {
+            if ($i === 0){
+                $sql = $sql . " $key=?";
+            } else {
+                $sql = $sql . ", $key=?";
+            }
+            $i++;
+        }
+        
+        $sql = $sql . " WHERE id=?";
+        $data['id'] = $id;
+        $stmt = executeQuery($sql, $data);
+        $id = $stmt->insert_id;
+        return $stmt->affected_rows;
+}
+
+$data = [
+    'username' => 'Master',
+    'admin'  => 1,
+    'email' => 'master@gmail.com',
+    'password' => 'Brows'
 ];
 
-$users = selectOne('users', $conditions);
-dd($users);
+$id = create('users', $data);
+dd($id);
 
