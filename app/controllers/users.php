9566fc85332d423db8cedb20e409999a26acca60
dancunrandy <dancunrandy@gmail.com>
2024-08-31 03:53:02 -0400

    encripted passwords

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,6 +1,15 @@
 <?php
+
+include(ROOT_PATH . "/app/database/db.php");
+
 if (isset($_POST['register-btn'])){
-    var_dump($_POST);
-    die();
+    unset($_POST['register-btn'], $_POST['passwordconf']);
+    $_POST['admin'] = 0;
+    
+    $_POST['password'] = password_hash($_POST['password'],PASSWORD_DEFAULT);
+    
+    $user_id = create('users',$_POST);
+    $user = selectOne('users',['id' => $user_id]);
+    dd($user);
 }
 ?>
