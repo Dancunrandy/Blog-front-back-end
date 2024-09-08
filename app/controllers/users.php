12ebf2b595d95cac86275d62acb76b16828a7a85
dancunrandy <dancunrandy@gmail.com>
2024-09-08 09:06:10 -0400

    dry code

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -7,6 +7,22 @@ $username = '';
 $email = '';
 $password = '';
 $passwordConf = '';
+$table = 'users';
+
+function loginUser($user){
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+    $_SESSION['admin'] = $user['admin'];
+    $_SESSION['message'] = 'logged In';
+    $_SESSION['type'] = 'success';
+    
+    if ($_SESSION['admin']) {
+        header('location:' .BASE_URL .'/admin/dashboard.php');
+    } else {
+        header('location:' .BASE_URL .'/index.php');
+    }
+    exit();
+}
 
 
 if (isset($_POST['register-btn'])){
@@ -18,24 +34,12 @@ if (isset($_POST['register-btn'])){
         
         $_POST['password'] = password_hash($_POST['password'],PASSWORD_DEFAULT);
         
-        $user_id = create('users',$_POST);
-        $user = selectOne('users',['id' => $user_id]);
-        
-        
+        $user_id = create($table,$_POST);
+        $user = selectOne($table,['id' => $user_id]);
+     
         //user logins
+        loginUser($user);
         
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['admin'] = $user['admin'];
-        $_SESSION['message'] = 'logged In';
-        $_SESSION['type'] = 'success';
-        
-        if ($_SESSION['admin']) {
-            header('location:' .BASE_URL .'/admin/dashboard.php');
-        } else {
-            header('location:' .BASE_URL .'/index.php');
-        }
-        exit();
     } else {
         $username = $_POST['username'];
         $email = $_POST['email'];
@@ -45,4 +49,25 @@ if (isset($_POST['register-btn'])){
     
 
 }
+
+if (isset($_POST['login-btn'])){
+    $errors = validateLogin($_POST);
+    
+    if (count($errors) === 0){
+        $user = selectOne($table, ['username' => $_POST['username']]);
+        
+        if ($user && password_verify($_POST['password'], $user['password'])) {
+            # login user, redirect...
+            loginUser($user);
+        } else {
+            array_push($errors, 'wrong credentials');
+        }
+        
+    }
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+}
+
 ?>
